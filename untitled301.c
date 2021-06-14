a01bf46b1a4613b16ae42ab897d09c66848072f5
Nt1128 <84721702+Nt1128@users.noreply.github.com>
2021-06-14 23:31:48 +0900

    Add files via upload

---
+++ untitled301.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  double result;
+  int a, b;
+
+  a = 2, b = 10;
+  result = pow(2, 10);
+
+  printf("1KB = %.lfBytes", result);
+
+  return 0;
+}
